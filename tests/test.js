ef727392bc6ecd215505adf8c0773f978aa667ef
Ivaylo Ivanov <ivchoyyyy@gmail.com>
2015-11-11 16:15:27 +0200

    added tests for class register

--- tests/test.js
+++ tests/test.js
@@ -3,34 +3,80 @@ let expect = require('chai').expect;
 let Err = require('../lib/errors.js');
 let errors = new Err();
 
-
 describe('Errors', () => {
 
-  it('Should throw an error', () => {
-    expect(() => { errors.error('Success', 1) }).to.throw(Error);    
-  });
+  describe('Register Success...', () => {
 
-  it('Should throw an error with message "Success" and number "1"', () => {
-    try {
-      errors.error('Success', 1);
-    } catch(e) {
-      expect(e.code).to.equal(1);
-      expect(e.message).to.equal('Success');
-    }
+    describe('Set error with number and description', () => {
+      it('Should return true', () => {
+        expect(errors.setWithNumber(1, 'Some message')).to.equal(true);
+      });      
+    });
 
-  });
+    describe('Get error message by error number', () => {
 
-  it('Should throw an error with message "Success #2" and number "2"', () => {
-    try {
-      errors.error({
-        name: 'FatalError',
-        defaultMessage: 'Success #2',
-        code: 2
+      it('Should return "Some message"', () => {
+        expect(errors.getByNumber(1)).to.equal('Some message');
+      }); 
+
+      it('Should return "Message"', () => {
+        errors.setWithNumber(2, 'Message');
+        expect(errors.getByNumber(2)).to.equal('Message');
+      });  
+
+    });
+
+    describe('Get all registered errors with numbers and descriptions', () => {
+      let should = `Should have two keys: "1" and "2" with 
+          value: "Some message" and "Message"`;
+      it(should, () => {
+        let result = errors.getAllErrors();
+        expect(result).to.have.all.keys('1', '2');
+        expect(result[1]).to.equal('Some message');
+        expect(result[2]).to.equal('Message');
       });
-    } catch(e) {
-      expect(e.code).to.equal(2);
-      expect(e.message).to.equal('Success #2');
-    }
+    });
+
+  });
+  
+  describe('Errors Success...', () => {
+
+    it('Should throw an error', () => {
+      expect(() => { errors.error('Success', 1) }).to.throw(Error);    
+    });
+
+    it('Should throw an error with message "Success" and number "1"', () => {
+      try {
+        errors.error('Success', 1);
+      } catch(e) {
+        expect(e.code).to.equal(1);
+        expect(e.message).to.equal('Success');
+        expect(e.explanation).to.equal('Some message');
+      }
+    });
+
+    it('Should throw an error with message "Success #2" and number "2"', () => {
+      try {
+        errors.error('Success #2', 2);
+      } catch(e) {
+        expect(e.code).to.equal(2);
+        expect(e.message).to.equal('Success #2');
+        expect(e.explanation).to.equal('Message');
+      }
+    });
+
+  });
+
+  let should = 'Throw an error';
+
+  describe('Register Fail...', () => {
+
+    describe('Register the same number twice', () => {
+      it(should, () => {
+        expect(() => { errors.setWithNumber(1, 'message') }).to.throw(Error);
+      });      
+    });
+
   });  
   
 });
